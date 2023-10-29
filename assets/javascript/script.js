cfb7c7e02460ec3d4da6f80285bcfca8dabb26d7
Ross Chandler <rosspchandler@gmail.com>
2023-10-29 11:26:47 +0000

    add function to change background color

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -16,3 +16,17 @@ function closeOptionsMenu() {
     let close = document.getElementById("hidden-menu");
     close.style.bottom = "-400px";
 }
+
+/*
+Javascript function used to change background-color from within the hidden menu 
+*/
+
+let color = document.getElementById('color-grid')
+
+let colorChoice = e => {
+    let result = document.body;
+    result.style.backgroundColor = `${e.target.id}`;
+}
+
+
+color.addEventListener("click", colorChoice);
