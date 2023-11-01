7f936290f7d22363a204ed3d6716875b74f50953
Ross Chandler <rosspchandler@gmail.com>
2023-11-01 20:14:20 +0000

    create image array and shuffle function

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,4 +1,40 @@
 /*
+Array for storing the tile image data
+*/
+
+const animalImages = () => [
+    {imgSrc:"assets/images/animal-set/cat.webp", name:"cat"},
+    {imgSrc:"assets/images/animal-set/cow.webp", name:"cow"},
+    {imgSrc:"assets/images/animal-set/dog.webp", name:"dog"},
+    {imgSrc:"assets/images/animal-set/fox.webp", name:"fox"},
+    {imgSrc:"assets/images/animal-set/horse.webp", name:"horse"},
+    {imgSrc:"assets/images/animal-set/mouse.webp", name:"mouse"},
+    {imgSrc:"assets/images/animal-set/pig.webp", name:"pig"},
+    {imgSrc:"assets/images/animal-set/sheep.webp", name:"sheep"},
+    {imgSrc:"assets/images/animal-set/cat.webp", name:"cat"},
+    {imgSrc:"assets/images/animal-set/cow.webp", name:"cow"},
+    {imgSrc:"assets/images/animal-set/dog.webp", name:"dog"},
+    {imgSrc:"assets/images/animal-set/fox.webp", name:"fox"},
+    {imgSrc:"assets/images/animal-set/horse.webp", name:"horse"},
+    {imgSrc:"assets/images/animal-set/mouse.webp", name:"mouse"},
+    {imgSrc:"assets/images/animal-set/pig.webp", name:"pig"},
+    {imgSrc:"assets/images/animal-set/sheep.webp", name:"sheep"},
+];
+
+//Randomise Function
+
+const shufffle = () => {
+    const tileImages = animalImages()
+;    tileImages.sort(() => Math.random() - 0.5);
+console.log(tileImages);
+};
+
+shufffle();
+
+
+
+
+/*
 Event Listener and functon to control hidden options menu
 */ 
 let openOptions = document.getElementById('open-options');
